410b3546ce78610bc2529d208a28f55f82d1c5fe
Paul Isaris <paulisaris@gmail.com>
2022-01-20 10:33:46 +0200

    Added dummy migration

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -1,6 +1,26 @@
 <?php
 
-/**
- * This file exists only to surpass the message of migration informing
- * that the 'personal_access_tokens' table does not exist.
- */
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonalAccessTokensTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        /**
+         * This file exists only to surpass the message of migration informing
+         * that the 'personal_access_tokens' table does not exist.
+         */
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+
+    }
+}
