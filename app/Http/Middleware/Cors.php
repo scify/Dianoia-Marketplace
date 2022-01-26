cb7fa74e27cd98e54a19a3d51ce8ee6b1d6d750a
Paul Isaris <paulisaris@gmail.com>
2022-01-26 14:52:02 +0200

    Commented out headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,9 +18,10 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         Log::info("CORS Middleware called!");
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+//        return $next($request)
+//            ->header('Access-Control-Allow-Origin', '*')
+//            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+//            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+        return $next($request);
     }
 }
