eda862f033cded3e0354b58385b80eba9222b03e
Paul Isaris <paulisaris@gmail.com>
2022-01-26 14:16:41 +0200

    Added Log info

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class Cors
 {
@@ -16,6 +17,7 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
+        Log::info("CORS Middleware called!");
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
