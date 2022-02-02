bc5e151b391f7715a0f41d4d612e0a44064dd03f
NikolasGialitsis <nikolasyal@gmail.com>
2022-02-02 19:45:11 +0200

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,6 @@ Route::middleware(['auth'])->group(function () {
     Route::post("/resources/approve/{id}", [ResourceController::class, 'approve'])->name('resources.approve');
     Route::post("/resources/reject/{id}", [ResourceController::class, 'reject'])->name('resources.reject');
     Route::post("/resources/report/{id}", [ResourceController::class, 'report'])->name('resources.report');
-    Route::post("/resources/hide/{id}", [ResourceController::class, 'hide'])->name('resources.hide');
     Route::post("/resources/submit", [ResourceController::class, 'submit'])->name('resources.submit');
     Route::put("/resources/update/{id}", [ResourceController::class, 'update'])->name('resources.update');
     Route::put("/resources/update_resource/{id}/{type_id}", [ResourceController::class, 'update_resource'])->name('resources.update_resource');
