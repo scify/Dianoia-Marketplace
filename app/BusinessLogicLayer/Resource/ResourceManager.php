a9474a29b936b2e9b9995392cdb84d0311c5b43d
Paul Isaris <paulisaris@gmail.com>
2022-06-14 10:26:55 +0300

    Bigfixing

--- app/BusinessLogicLayer/Resource/ResourceManager.php
+++ app/BusinessLogicLayer/Resource/ResourceManager.php
@@ -3,11 +3,7 @@
 
 namespace App\BusinessLogicLayer\Resource;
 
-use App\Models\Reports;
-use http\Exception\InvalidArgumentException;
-use Illuminate\Support\Str;
 use App\Models\Resource\Resource;
-use App\Models\User;
 use App\Repository\ContentLanguageLkpRepository;
 use App\Repository\DifficultiesLkpRepository;
 use App\Repository\ReportsRepository;
@@ -17,10 +13,10 @@ use App\Repository\Resource\ResourceTypeLkpRepository;
 use App\Repository\Resource\ResourceTypesLkp;
 use App\ViewModels\CreateEditResourceVM;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
+use InvalidArgumentException;
 
 
 class ResourceManager {
@@ -28,15 +24,14 @@ class ResourceManager {
     protected ResourceRepository $resourceRepository;
     protected ContentLanguageLkpRepository $contentLanguageLkpRepository;
     protected DifficultiesLkpRepository $difficultiesLkpRepository;
-    protected ResourceTypesLkp $resourceTypesLkp;
     protected ResourceTypeLkpRepository $resourceTypeLkpRepository;
     protected ReportsRepository $reportsRepository;
 
-    public function __construct(ResourceRepository $resourceRepository,
-                                ContentLanguageLkpRepository  $contentLanguageLkpRepository,
-                                DifficultiesLkpRepository $difficultiesLkpRepository,
-                                ResourceTypeLkpRepository $resourceTypeLkpRepository,
-                                ReportsRepository $reportsRepository
+    public function __construct(ResourceRepository           $resourceRepository,
+                                ContentLanguageLkpRepository $contentLanguageLkpRepository,
+                                DifficultiesLkpRepository    $difficultiesLkpRepository,
+                                ResourceTypeLkpRepository    $resourceTypeLkpRepository,
+                                ReportsRepository            $reportsRepository
     ) {
         $this->resourceRepository = $resourceRepository;
         $this->contentLanguageLkpRepository = $contentLanguageLkpRepository;
@@ -60,11 +55,10 @@ class ResourceManager {
         $contentLanguages = $this->getContentLanguagesForResources();
         $difficulties = $this->getDifficultiesForResources();
         $lang = app()->getLocale();
-        $types =  $this->getResourceTypes();
+        $types = $this->getResourceTypes();
 
         $type_ids = $types->map(
-            function ($type_entry)
-            {
+            function ($type_entry) {
                 return $type_entry->id;
             }
         );
@@ -85,63 +79,53 @@ class ResourceManager {
     }
 
 
-
-
-    public function getResourceTypes()
-    {
-        $ret =  $this->resourceTypeLkpRepository->all();
+    public function getResourceTypes() {
+        $ret = $this->resourceTypeLkpRepository->all();
         return $ret;
     }
 
 
-    public function getDifficultiesForResources()
-    {
+    public function getDifficultiesForResources() {
         return $this->difficultiesLkpRepository->all();
     }
 
 
-    public function getContentLanguagesForResources()
-    {
+    public function getContentLanguagesForResources() {
         return $this->contentLanguageLkpRepository->all();
     }
 
 
-
-    public function getResource($id){
+    public function getResource($id) {
         return $this->resourceRepository->find($id);
     }
 
-    //TODO: remove pending as a default value after admin approval has been implemented
-    public function getResources(int $lang_id = null, $user_id = null, array $status_ids=[], array $difficulties=null, array $type_ids=null, array $ratings=null) {
-        if($status_ids == [])
-            $status_ids=[ResourceStatusesLkp::APPROVED];
-        $ret = $this->resourceRepository->getResources($user_id, $lang_id, $status_ids, $difficulties, $type_ids, $ratings);
-        return $ret;
+    public function getResources(int $lang_id = null, $user_id = null, array $status_ids = [], array $difficulties = null, array $type_ids = null, array $ratings = null) {
+        if ($status_ids == [])
+            $status_ids = [ResourceStatusesLkp::APPROVED];
+        return $this->resourceRepository->getResources($user_id, $lang_id, $status_ids, $difficulties, $type_ids, $ratings);
     }
 
-    public function rejectResource($id){
+    public function rejectResource($id) {
         return $this->resourceRepository->update(['status_id' => ResourceStatusesLkp::REJECTED], $id);
     }
 
-    public function approveResource($id){
+    public function approveResource($id) {
         return $this->resourceRepository->update(['status_id' => ResourceStatusesLkp::APPROVED], $id);
     }
 
 
     /**
-     * @throws FileNotFoundException
      */
-    public function storeResource(Request $request)
-    {
+    public function storeResource(Request $request) {
         $storeArr = [
             'name' => $request['name'],
             'slug' => null,
-            'lang_id' =>  $request['lang'],
+            'lang_id' => $request['lang'],
             'img_path' => null,
             'pdf_path' => null,
             'type_id' => $request['type_id'],
             'difficulty_id' => $request['difficulty_id'],
-            'status_id' =>   ResourceStatusesLkp::CREATED_PENDING_APPROVAL,
+            'status_id' => ResourceStatusesLkp::CREATED_PENDING_APPROVAL,
             'creator_user_id' => Auth::id(),
             'admin_user_id' => null,
             'description' => $request['description']
@@ -149,16 +133,14 @@ class ResourceManager {
 
         $resource = $this->resourceRepository->create($storeArr);
         $resourceFileManager = new ResourceFileManager();
-        try{
+        try {
             $img_path = $resourceFileManager->saveImage($resource->id, $request);
-        }
-        catch(FileNotFoundException $e){
+        } catch (FileNotFoundException $e) {
             $img_path = null;
         }
-        try{
+        try {
             $pdf_path = $resourceFileManager->savePdf($resource->id, $request);
-        }
-        catch(FileNotFoundException $e){
+        } catch (FileNotFoundException $e) {
             $pdf_path = null;
         }
 
@@ -174,14 +156,10 @@ class ResourceManager {
     /**
      * @throws FileNotFoundException
      */
-    public function updateResource($request, $id)
-    {
+    public function updateResource($request, $id) {
         $storeArr = [
             "name" => $request['name'],
             "slug" => $request['slug'],
-//            "lang_id" => $request['lang'],
-            "img_path" => null,
-            "pdf_path" => null,
             "description" => $request['description'],
             "lang_id" => $request['lang'],
             "type_id" => $request['type_id'],
@@ -213,20 +191,18 @@ class ResourceManager {
     }
 
 
-    public function reportResource($id, $reporting_user_id, $reportReason, $reportComment)
-    {
+    public function reportResource($id, $reporting_user_id, $reportReason, $reportComment) {
         $storeArr = [
             'resource_id' => $id,
             'reporting_user_id' => $reporting_user_id,
-            'reason' =>  $reportReason,
+            'reason' => $reportReason,
             'comment' => $reportComment,
         ];
-        $reports = $this->reportsRepository->create($storeArr);
+        return $this->reportsRepository->create($storeArr);
     }
 
 
-    public function destroyResource($id)
-    {
+    public function destroyResource($id) {
         $resource = $this->resourceRepository->find($id);
         $resourceFileManager = new ResourceFileManager();
         $resourceFileManager->deleteResourceAudio($resource);
@@ -235,11 +211,11 @@ class ResourceManager {
     }
 
 
-    public function cloneResource($id, $newParentId){
+    public function cloneResource($id, $newParentId) {
         $resource = $this->resourceRepository->find($id);
         $fileManager = new ResourceFileManager();
         $storeArr = [
-            "name" =>  $resource->name,
+            "name" => $resource->name,
             "img_path" => $fileManager->cloneResourceToDirectory(basename($resource->img_path), "image"),
             "audio_path" => $resource->audio_path ? $fileManager->cloneResourceToDirectory(basename($resource->audio_path), "audio") : null,
             'resource_parent_id' => $newParentId ?: null,
@@ -249,91 +225,90 @@ class ResourceManager {
         return $this->resourceRepository->create($storeArr);
     }
 
-    public function storeOrUpdateAverageResourceRating($id, $avg_rating){
+    public function storeOrUpdateAverageResourceRating($id, $avg_rating) {
         return $this->resourceRepository->update([
             'avg_rating' => $avg_rating],
             $id);
     }
 
-    public function getTransformExercisesForMobileApp($paginated)
-    {
+    public function getTransformExercisesForMobileApp($paginated) {
 
 
-        $transformed =  $paginated->through(function ($exercise)  {
-                $keys =  array_keys($exercise->toArray());
-                $exercise['slug'] = $exercise->slug;
-                $exercise['title'] = $exercise->name;
-                $exercise['description'] = $exercise->description;
+        $transformed = $paginated->through(function ($exercise) {
+            $keys = array_keys($exercise->toArray());
+            $exercise['slug'] = $exercise->slug;
+            $exercise['title'] = $exercise->name;
+            $exercise['description'] = $exercise->description;
 
-                switch ($exercise->type_id){
-                    case ResourceTypesLkp::ATTENTION:
-                        $exercise['category']  = "focus_activities";
-                        break;
-                    case ResourceTypesLkp::MEMORY:
-                        $exercise['category']  = "memory_activities";
-                        break;
-                    case ResourceTypesLkp::REASON:
-                        $exercise['category']  = "think_activities";
-                        break;
-                    case ResourceTypesLkp::EXECUTIVE:
-                        $exercise['category']  = "executive_activities";
-                        break;
-                    case ResourceTypesLkp::CARER:
-                        $exercise['category'] = "carer_activities";
-                        break;
-                    default:
-                        $exercise['category'] = "";
+            switch ($exercise->type_id) {
+                case ResourceTypesLkp::ATTENTION:
+                    $exercise['category'] = "focus_activities";
+                    break;
+                case ResourceTypesLkp::MEMORY:
+                    $exercise['category'] = "memory_activities";
+                    break;
+                case ResourceTypesLkp::REASON:
+                    $exercise['category'] = "think_activities";
+                    break;
+                case ResourceTypesLkp::EXECUTIVE:
+                    $exercise['category'] = "executive_activities";
+                    break;
+                case ResourceTypesLkp::CARER:
+                    $exercise['category'] = "carer_activities";
+                    break;
+                default:
+                    $exercise['category'] = "";
+            }
+
+            $exercise['full_text'] = "";
+            $exercise['video_url'] = "";
+            $exercise['image_url'] = $exercise->img_path == null ? "" : $exercise->img_path;
+            $exercise['instructions'] = ["Κάντε click στο σύνδεσμο για να δείτε το έγγραφο"];
+            $exercise['link'] = config("app.url") . '/storage/' . $exercise->pdf_path;
+            $exercise['difficulty_level'] = 'difficulty_level_' . ($exercise->difficulty_id + 1);
+            $exercise['created_by'] = $this->resourceRepository->where(
+                ['id' => $exercise->id], array('*'), ['creator'])->first()->creator->name;
+            $exercise['rating'] = $exercise->avg_rating;
+            foreach ($keys as $key) {
+                if ($key != 'slug' and $key != 'description') {
+                    unset($exercise[$key]);
                 }
-
-                $exercise['full_text'] = "";
-                $exercise['video_url'] = "";
-                $exercise['image_url'] = $exercise->img_path == null ? "" : $exercise->img_path;
-                $exercise['instructions'] = ["Κάντε click στο σύνδεσμο για να δείτε το έγγραφο"];
-                $exercise['link'] = config("app.url") . '/storage/'.$exercise->pdf_path;
-                $exercise['difficulty_level'] = 'difficulty_level_'.($exercise->difficulty_id+1);
-                $exercise['created_by'] =  $this->resourceRepository->where(
-                        ['id'=>$exercise->id], array('*'),'creator')->first()->creator->name;
-                $exercise['rating'] = $exercise->avg_rating;
-                foreach($keys as $key){
-                    if($key != 'slug' and $key != 'description'){
-                        unset($exercise[$key]);
-                    }
-                }
-                return $exercise;
+            }
+            return $exercise;
         })->toArray();
         $transformed['data'] = array_values(array_filter($transformed['data']));
         return $transformed;
     }
 
-    public function getPaginatedResourcesForMobile($requestLanguage=null, $requestCategory=null){
+    public function getPaginatedResourcesForMobile($requestLanguage = null, $requestCategory = null) {
 
         $whereArray = [
             'lang_id' => null,
             'type_id' => null
         ];
 
-        if($requestLanguage){
+        if ($requestLanguage) {
             $contentLanguages = $this->getContentLanguagesForResources();
-            foreach($contentLanguages as $lang) {
+            foreach ($contentLanguages as $lang) {
                 if ($lang->code == $requestLanguage) {
                     $whereArray['lang_id'] = $lang->id;
                     break;
                 }
             }
         }
-        if($requestCategory) {
+        if ($requestCategory) {
             switch ($requestCategory) {
                 case "focus_activities":
-                    $whereArray['type_id']  = [ResourceTypesLkp::ATTENTION];
+                    $whereArray['type_id'] = [ResourceTypesLkp::ATTENTION];
                     break;
                 case "memory_activities":
                     $whereArray['type_id'] = [ResourceTypesLkp::MEMORY];
                     break;
                 case "think_activities":
-                    $whereArray['type_id']  = [ResourceTypesLkp::REASON];
+                    $whereArray['type_id'] = [ResourceTypesLkp::REASON];
                     break;
                 case "executive_activities":
-                    $whereArray['type_id']  = [ResourceTypesLkp::EXECUTIVE];
+                    $whereArray['type_id'] = [ResourceTypesLkp::EXECUTIVE];
                     break;
                 case "carer_activities":
                     $whereArray['type_id'] = [ResourceTypesLkp::CARER];
@@ -346,19 +321,17 @@ class ResourceManager {
 
         $paginated = $this->resourceRepository->getResources(
             null, $whereArray['lang_id'], null, null, $whereArray['type_id'], null, 10, True);
-
-//        $paginated->setCollection($filteredExercises->get());
         $paginated->appends(['lang' => $requestLanguage])->links();
         $paginated->appends(['category' => $requestCategory])->links();
 
         return $this->getTransformExercisesForMobileApp($paginated);
     }
 
-    public function getReportedExercises(){
+    public function getReportedExercises() {
 
-        $reports =  $this->reportsRepository->all();
+        $reports = $this->reportsRepository->all();
         $resourcesWithReportInfo = Collection::empty();
-        foreach($reports as $report){
+        foreach ($reports as $report) {
             $resource = $this->resourceRepository->find($report->resource_id);
             $resource->reportData = $report;
             $resource->creator = $report->creator;
