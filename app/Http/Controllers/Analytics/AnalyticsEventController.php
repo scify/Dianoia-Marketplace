8854af89c81a038f05b3dacb6af8252084852b87
Paul Isaris <paulisaris@gmail.com>
2022-12-07 14:35:10 +0200

    Test log

--- app/Http/Controllers/Analytics/AnalyticsEventController.php
+++ app/Http/Controllers/Analytics/AnalyticsEventController.php
@@ -6,6 +6,7 @@ use App\BusinessLogicLayer\Shapes\ShapesIntegrationManager;
 use App\Http\Controllers\Controller;
 use App\Repository\Analytics\AnalyticsEventRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AnalyticsEventController extends Controller {
     protected $analyticsEventRepository;
@@ -18,6 +19,7 @@ class AnalyticsEventController extends Controller {
     }
 
     public function store(Request $request) {
+        Log::info("test");
         $request->validate([
             'name' => 'required',
             'source' => 'required'
