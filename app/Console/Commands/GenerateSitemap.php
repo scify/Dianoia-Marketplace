e2cdf888a9d62ed674e373a051b497db66d9a3a8
Paul Isaris <paulisaris@gmail.com>
2022-12-07 15:55:32 +0200

    Set frequencies

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -38,15 +38,15 @@ class GenerateSitemap extends Command {
      */
     public function handle() {
         $sitemapGenerator = Sitemap::create();
-        $sitemapGenerator->add(Url::create('/')->setPriority(1.0)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/login')->setPriority(0.9)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/register')->setPriority(0.9)->addImage(asset('img/carer-help-scaled.webp'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/about')->setPriority(0.8)->addImage(asset('img/title-image-scaled.webp'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/content-guidelines')->setPriority(0.8)->addImage(asset('img/header-image-scaled.webp'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/terms-of-use')->setPriority(0.2)->addImage(asset('img/carer-help-scaled.webp'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/tutorial')->setPriority(0.9)->addImage(asset('img/grandmother-flowers-scaled.webp'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/privacy-policy')->setPriority(0.2)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image'));
-        $sitemapGenerator->add(Url::create('/resources/display_exercises')->setPriority(0.9)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image'));
+        $sitemapGenerator->add(Url::create('/')->setPriority(1.0)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+        $sitemapGenerator->add(Url::create('/login')->setPriority(0.9)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY));
+        $sitemapGenerator->add(Url::create('/register')->setPriority(0.9)->addImage(asset('img/carer-help-scaled.webp'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY));
+        $sitemapGenerator->add(Url::create('/about')->setPriority(0.8)->addImage(asset('img/title-image-scaled.webp'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY));
+        $sitemapGenerator->add(Url::create('/content-guidelines')->setPriority(0.8)->addImage(asset('img/header-image-scaled.webp'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY));
+        $sitemapGenerator->add(Url::create('/terms-of-use')->setPriority(0.2)->addImage(asset('img/carer-help-scaled.webp'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY));
+        $sitemapGenerator->add(Url::create('/tutorial')->setPriority(0.9)->addImage(asset('img/grandmother-flowers-scaled.webp'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+        $sitemapGenerator->add(Url::create('/privacy-policy')->setPriority(0.2)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY));
+        $sitemapGenerator->add(Url::create('/resources/display_exercises')->setPriority(0.9)->addImage(asset('img/dianoia_logo.png'), 'Dianoia Marketplace image')->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
         $sitemapGenerator->writeToFile(public_path('sitemap.xml'));
         return CommandAlias::SUCCESS;
     }
