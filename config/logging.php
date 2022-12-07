4da55afd6e4568e36e54ee420f2cbea8e2312bfc
Paul Isaris <paulisaris@gmail.com>
2022-12-07 14:33:32 +0200

    Changed logging default stack to daily

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'sentry'],
             'ignore_exceptions' => false,
         ],
 
