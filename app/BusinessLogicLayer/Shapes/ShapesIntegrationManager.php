32bddc0cc56588f82c0ba5fd72ab5789c8a20043
Paul Isaris <paulisaris@gmail.com>
2023-01-02 15:15:56 +0200

    Added log line

--- app/BusinessLogicLayer/Shapes/ShapesIntegrationManager.php
+++ app/BusinessLogicLayer/Shapes/ShapesIntegrationManager.php
@@ -139,6 +139,7 @@ class ShapesIntegrationManager {
             'Accept' => 'application/json',
         ])
             ->post($this->datalakeAPIUrl . '/marketplace');
+        Log::info($response->body());
         if (!$response->ok()) {
             throw new Exception(json_decode($response->body()));
         }
