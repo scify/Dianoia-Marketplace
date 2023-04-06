c7a8ac510f50e636908d72a8b8f6ab3dc5d69cc3
Paul Isaris <paulisaris@gmail.com>
2023-04-06 16:37:28 +0300

    Added new Sentry configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,25 +5,32 @@ import Vue from "vue";
 require("./bootstrap");
 window.$ = window.jQuery = $;
 import route from "./backend-route";
+
 window.route = route;
 import store from "./store/store";
 
 import getLodash from "lodash/get";
 import eachRightLodash from "lodash/eachRight";
 import replaceLodash from "lodash/replace";
+import * as Sentry from "@sentry/browser";
 
+if (import.meta.env.VITE_SENTRY_DSN_PUBLIC) {
+    Sentry.init({
+        dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+    });
+}
 
 window.translate = function (string, args) {
-	let value = getLodash(window.i18n, string);
+    let value = getLodash(window.i18n, string);
 
-	eachRightLodash(args, (paramVal, paramKey) => {
-		value = replaceLodash(value, `:${paramKey}`, paramVal);
-	});
-	return value;
+    eachRightLodash(args, (paramVal, paramKey) => {
+        value = replaceLodash(value, `:${paramKey}`, paramVal);
+    });
+    return value;
 };
 
 Vue.prototype.trans = (string, args) => {
-	return window.translate(string, args);
+    return window.translate(string, args);
 };
 
 Vue.component("modal-component", require("./vue-components/common/ModalComponent").default);
@@ -32,27 +39,27 @@ Vue.component("exercise-component", require("./vue-components/resources/Exercise
 
 
 new Vue({
-	el: "#app",
-	store: store
+    el: "#app",
+    store: store
 });
 (function ($) {
 
-	let init = function () {
-		closeDismissableAlerts();
-	};
+    let init = function () {
+        closeDismissableAlerts();
+    };
 
-	let closeDismissableAlerts = function () {
-		setTimeout(function () {
-			/*Close any flash message after some time*/
-			$(".alert-dismissible").fadeTo(4000, 500).slideUp(500);
-		}, 3000);
-	};
+    let closeDismissableAlerts = function () {
+        setTimeout(function () {
+            /*Close any flash message after some time*/
+            $(".alert-dismissible").fadeTo(4000, 500).slideUp(500);
+        }, 3000);
+    };
 
-	$(function () {
-		$(document).ready(function () {
-			init();
-		});
-	});
+    $(function () {
+        $(document).ready(function () {
+            init();
+        });
+    });
 
 })(window.jQuery);
 
